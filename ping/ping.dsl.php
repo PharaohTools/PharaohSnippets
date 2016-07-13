387402791b6c9d61ae98a063412300ee4a853fe2
Pharaoh Dave <dave@pharaohtools.com>
2016-07-13 02:18:48 +0100

    ping for multi targets

--- ping/ping.dsl.php
+++ ping/ping.dsl.php
@@ -1,15 +1,14 @@
 Ping once
-  label "Ping google once"
-  target www.google.com
-  interval 3
+  label "Ping google and yahoo once"
+  target www.google.com,www.yahoo.com
 
 Ping ten
   label "Ping google ten times"
   target www.google.com
-  interval 3
+  interval 1
 
 Ping until-responding
   label "Ping google until it responds"
   target www.google.com
-  interval 3
-  max-wait 30
+  interval 1
+  max-wait 10
