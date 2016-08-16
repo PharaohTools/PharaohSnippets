77eb76404101080a063887a354a1ebe457a0d3e6
David Amanshia <phpengine@pharaohtools.com>
2016-08-16 14:50:49 +0100

    rackspacec domain

---
+++ ensure_domin_record_rackspace/ensure_domain.dsl.php
@@ -0,0 +1,6 @@
+Rackspace ensure-domain-exists
+  guess true
+  domain-name pharaohtools.com
+  domain-ttl 3600
+  domain-comment "The Pharaoh Tools group of Domains"
+  domain-email "admin@pharaohtools.com"
