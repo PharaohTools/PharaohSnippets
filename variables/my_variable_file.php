93eb7b02c54dd8b7cef21bfbc702f7de26ae2a64
David Amanshia <phpengine@pharaohtools.com>
2016-10-09 07:51:52 +0100

    demo code using variables in usage examples

---
+++ variables/my_variable_file.php
@@ -0,0 +1,7 @@
+<?php
+
+$variables = array(
+    "my-first-test-variable" => "First Variable",
+    "my-second-test-variable" => "Another Variable",
+    "current-execution-time" => "Current Execution Time is ".time(),
+) ;
---
+++ variables/variables.dsl.php
@@ -0,0 +1,8 @@
+Logging log
+  log-message "Here is a Variable: {{{ var::my-first-test-variable }}}"
+
+Logging log
+  log-message "Here is another Variable: {{{ var::my-second-test-variable }}}"
+
+Logging log
+  log-message "And from a method in the Variable file: {{{ var::current-execution-time }}}"
