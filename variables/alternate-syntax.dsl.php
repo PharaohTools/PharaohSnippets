029724e6d1d1c7720041d093b509f6bc3bb95c50
David Amanshia <phpengine@pharaohtools.com>
2016-10-20 06:59:07 +0100

    alternate syntax variables file

---
+++ variables/alternate-syntax.dsl.php
@@ -0,0 +1,11 @@
+Logging log
+  source AutopilotDSL
+  log-message "Here is a Variable: $$my-first-test-variable"
+
+Logging log
+  source AutopilotDSL
+  log-message "Here is another Variable: $$my-second-test-variable"
+
+Logging log
+  source AutopilotDSL
+  log-message "Two in a line: 1) $$my-first-test-variable 2) $$my-second-test-variable"
