ee7c5e17edb57cd5a20ce2dbf90cb69a6708bac9
David Amanshia <phpengine@pharaohtools.com>
2016-10-20 06:58:38 +0100

    reformatted vars so one can be used in another

--- variables/my_variable_file.php
+++ variables/my_variable_file.php
@@ -1,7 +1,6 @@
 <?php
 
-$variables = array(
-    "my-first-test-variable" => "First Variable",
-    "my-second-test-variable" => "Another Variable",
-    "current-execution-time" => "Current Execution Time is ".time(),
-) ;
+$variables = array();
+$variables["my-first-test-variable"] = "First Variable" ;
+$variables["my-second-test-variable"] = "Another Variable" ;
+$variables["current-execution-time"] = "Current Execution Time is ".time() ;
